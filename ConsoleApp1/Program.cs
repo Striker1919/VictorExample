43b46a2693d5766ed33806ffe552e2e5594ed235
Striker1919 <crazytacoman888@gmail.com>
2019-12-22 13:32:48 -0800

    leon commited this LOL ROFL

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            int number = 5;
+            Console.WriteLine(number);
+            number += 10;
+            Console.WriteLine(number);
+            Console.WriteLine("Hello World!2");
+        }
+    }
+}
